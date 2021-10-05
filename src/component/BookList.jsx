b4fa19a9240d63e8ad44d7beec6fe00dfddd7eda
Billions94 <E.a.egbedi@gmail.com>
2021-10-05 18:45:29 +0200

    commit

--- src/component/BookList.jsx
+++ src/component/BookList.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import FormControl from "react-bootstrap/FormControl";
 import Row from "react-bootstrap/Row";
@@ -9,57 +9,57 @@ import SingleBook from "./SingleBook";
 // Book Data
 import Horror from "../data/horror.json";
 
-class BookList extends Component {
-  state = {
-    title: "",
-    queryBooks: Horror,
-    selectedBook: null,
-  };
+const BookList = () => {
 
-  filterBooks = (query) => {
+
+const [title, setTitle] = useState('')
+const [queryBooks, setQueryBooks] = useState(Horror)
+const [selectedBook, setSelectedBook] = useState(null)
+
+  const filterBooks = (query) => {
     let queryBooks = Horror.filter((book) =>
       book.title.toLowerCase().includes(query.toLowerCase())
     );
-    this.setState({ queryBooks });
+    setQueryBooks({ queryBooks });
   };
 
-  render() {
+
     return (
       <Container className="mb-5 mt-5 fluid">
         <div className="d-flex justify-content-center mb-3">
           <FormControl
             className="input"
             placeholder="Search your favorite horror book..."
-            value={this.state.title}
+            value={title}
             onChange={(e) => {
-              this.setState({ title: e.target.value });
-              this.filterBooks(e.target.value);
+                setTitle(e.target.value);
+                filterBooks(e.target.value);
             }}
           />
         </div>
         <Row>
             <Col md={9}>
                 <div className="d-flex" style={{width:"100%"}}>
-                {this.state.queryBooks.map((book) => (
+                {
+                    queryBooks.map((book) => (
                     <Col key={book.asin} >
                     <SingleBook book={book}
-                    selectedBook={this.state.selectedBook}
-                    changeSelectedBook={asin => this.setState({
-                        selectedBook: asin
-                    })} />
+                    selectedBook={selectedBook}
+                    changeSelectedBook={asin => setSelectedBook(asin)} />
                     </Col>
                 ))}
                 </div>
             </Col>
             <Col>
-                <div className="mt-4 mb-5 ml-2"  style={{ minWidth: "7rem" }}>
-                    <Comments asin={this.state.selectedBook} />
-                </div>
+               { selectedBook && <div className="mt-4 mb-5 ml-2"  style={{ minWidth: "7rem" }}>
+                    <Comments asin={selectedBook} />
+                </div>}
             </Col>
          </Row>   
       </Container>
     );
-  }
+  
+ 
 }
 
 export default BookList;
--- src/component/SingleBook.jsx
+++ src/component/SingleBook.jsx
@@ -1,40 +1,33 @@
 import Card from "react-bootstrap/Card";
-import { Component } from "react";
+// import { Component } from "react";
 import Button from "react-bootstrap/Button";
 
-
-class SingleBook extends Component {
-
-
-  render() {
-    return (
-      <>
-        <div className="mt-4 mb-5 ml-2">
-          <Card
-            onClick={() => this.props.changeSelectedBook(this.props.book.asin)}
-            key={this.props.book.asin}
-            style={{ width: "16rem" }}
-          >
-            <Card.Img
-              variant="top"
-              src={this.props.book.img}
-              style={{ height: "20rem" }}
-            />
-            <Card.Body>
-              <Card.Title className="clamp">{this.props.book.title}</Card.Title>
-              <h5 className="d-inline-block">
-                Category: {this.props.book.category}
-              </h5>
-              <div className="d-flex justify-content-between">
-                <Button variant="warning">Buy</Button>
-                <div>{"€" + this.props.book.price}</div>
-              </div>
-            </Card.Body>
-          </Card>
-        </div>      
-      </>
-    );
-  }
-}
+const SingleBook = (props) => {
+  return (
+    <>
+      <div className="mt-4 mb-5 ml-2">
+        <Card
+          onClick={() => props.changeSelectedBook(props.book.asin)}
+          key={props.book.asin}
+          style={{ width: "16rem" }}
+        >
+          <Card.Img
+            variant="top"
+            src={props.book.img}
+            style={{ height: "20rem" }}
+          />
+          <Card.Body>
+            <Card.Title className="clamp">{props.book.title}</Card.Title>
+            <h5 className="d-inline-block">Category: {props.book.category}</h5>
+            <div className="d-flex justify-content-between">
+              <Button variant="warning">Buy</Button>
+              <div>{"€" + props.book.price}</div>
+            </div>
+          </Card.Body>
+        </Card>
+      </div>
+    </>
+  );
+};
 
 export default SingleBook;
